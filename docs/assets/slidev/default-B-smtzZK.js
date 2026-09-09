98dfa210d3bf6d81d373d5f893f245eedc30358e
Evgeny Erman <evgeny@razum.life>
2026-09-09 08:55:45 +0000

    Add built slides in docs/ for GitHub Pages

---
+++ docs/assets/slidev/default-B-smtzZK.js
@@ -0,0 +1,1 @@
+import{B as e,U as t,x as n}from"../modules/shiki-Dw2-XCmS.js";import{ht as r}from"../index--LLXHpUd.js";var i={},a={class:`slidev-layout default`};function o(r,i){return e(),n(`div`,a,[t(r.$slots,`default`)])}var s=r(i,[[`render`,o]]);export{s as t};
